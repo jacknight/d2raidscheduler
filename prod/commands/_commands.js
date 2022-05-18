03cc31a60a132d7392095ad6e48cbc2ec59a04eb
jacknight <jacknight@gmail.com>
2022-05-19 00:59:47 +0300

    Remove prod folder and add to gitignore

--- prod/commands/_commands.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const raid_1 = __importDefault(require("./raid"));
-const commandList = [raid_1.default];
-exports.default = commandList;
